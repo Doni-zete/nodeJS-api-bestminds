4ce561016e565ee347901477191799bbae05a362
doni-zete <donizete112@gmail.com>
2024-02-15 23:01:31 -0300

    alterando .env

--- src/databases/database.js
+++ src/databases/database.js
@@ -2,41 +2,41 @@ import { createConnection, createPool } from 'mysql2/promise'
 import { criaTabelaProdutos } from '../models/produtoModel.js'
 
 async function connectToDatabase() {
- const dbConfig = {
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  port: process.env.DB_PORT,
-  password: process.env.DB_PASSWORD,
- }
+    const dbConfig = {
+        host: process.env.MYSQLHOST,
+        user: process.env.MYSQLUSER,
+        port: process.env.MYSQLPORT,
+        password: process.env.MYSQLPASSWORD,
+    }
 
- try {
-  const connection = await createConnection(dbConfig)
+    try {
+        const connection = await createConnection(dbConfig)
 
-  await connection.query(
-   `CREATE DATABASE IF NOT EXISTS ${process.env.DB_DATABASE}`
-  )
-  console.log('Banco de dados criado ou já existente.')
+        await connection.query(
+            `CREATE DATABASE IF NOT EXISTS ${process.env.MYSQLDATABASE}`
+        )
+        console.log('Banco de dados criado ou já existente.')
 
-  await connection.query(`USE ${process.env.DB_DATABASE}`)
+        await connection.query(`USE ${process.env.MYSQLDATABASE}`)
 
-  await connection.end()
+        await connection.end()
 
-  const pool = createPool({
-   ...dbConfig,
-   database: process.env.DB_DATABASE,
-  })
+        const pool = createPool({
+            ...dbConfig,
+            database: process.env.MYSQLDATABASE,
+        })
 
-  const conexaoPrincipal = await pool.getConnection()
+        const conexaoPrincipal = await pool.getConnection()
 
-  console.log('MySQL conectado')
+        console.log('MySQL conectado')
 
-  await criaTabelaProdutos(conexaoPrincipal)
+        await criaTabelaProdutos(conexaoPrincipal)
 
-  return conexaoPrincipal
- } catch (error) {
-  console.error('Erro na conexão com o banco de dados MySQL:', error.message)
-  throw error
- }
+        return conexaoPrincipal
+    } catch (error) {
+        console.error('Erro na conexão com o banco de dados MySQL:', error.message)
+        throw error
+    }
 }
 
 export { connectToDatabase }
